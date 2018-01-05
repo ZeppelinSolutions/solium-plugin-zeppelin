df8eff561d687116b55e6e072bc6a9121724cdc1
Leo Arias <leo.arias@canonical.com>
2018-01-05 07:53:10 +0000

    rules: add the no-modifiers-without-revert rule

---
+++ rules/no-modifiers-without-revert.js
@@ -0,0 +1,57 @@
+/**
+ * @fileoverview Disallow modifiers without revert
+ */
+
+"use strict";
+
+module.exports = {
+    meta: {
+        docs: {
+            recommended: true,
+            type: "warning",
+            description: "Disallow modifiers without revert"
+        },
+        schema: []
+    },
+
+    create: function(context) {
+        function inspectModifierDeclaration(emitted) {
+            if (emitted.exit) {
+                return;
+            }
+            const node = emitted.node;
+            if (!hasRevertChild(node)) {
+                context.report({
+                    node: node,
+                    message: `Modifier '${node.name}' without revert.`
+                });
+            }
+        }
+
+        function hasRevertChild(node) {
+            // XXX Recursive function.
+            // --elopio - 20180105
+            if (Array.isArray(node)) {
+                return node.some(element => hasRevertChild(element));
+            } else if (node !== null && typeof node === "object") {
+                return isRevertExpression(node) ||
+                    Object.keys(node).some(key => {
+                        if (key !== "parent") {
+                            return hasRevertChild(node[key]);
+                        }
+                    });
+            }
+        }
+
+        function isRevertExpression(statement) {
+            return statement.type === "ExpressionStatement" &&
+                statement.expression.type === "CallExpression" &&
+                (statement.expression.callee.name === "require" ||
+                 statement.expression.callee.name === "revert");
+        }
+
+        return {
+            ModifierDeclaration: inspectModifierDeclaration
+        };
+    }
+};
